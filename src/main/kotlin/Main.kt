cb828d9c0631ada7ebfcd415abbaea37b0d00bd7
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-05-28 18:01:47 -0500

    Clase

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,27 @@
 package org.example
 
 fun main(args: Array<String>){
-    val nombre = "li"
+    val nombreColor = "Carmesi"
 
-    if (nombre.isNotEmpty()) println("El largo de nuestra variable nombre es ${nombre.length}") else println("Error , la variable esta vacia")
+    when (nombreColor) {
+        "Amarillo" -> println("EL amarillo es el color de la alegria")
+        "Rojo" , "Carmesi" -> println("Este color simboliza el calor")
+        else -> println("Error. No tengo informacion del color")
+    }
 
-    val mensaje : String = if (nombre.length > 4 ){
-        "Tu nombre es largo!"
-    } else if(nombre.isEmpty()){
-        "Nombre esta vacio"
-    } else {
-        "Tienes un nombre corto"
+    val code = 200
+    when(code){
+        in 200..299 -> println("Todo ha ido bien")
+        in 400..500 -> println("Algo ha fallado")
+        else -> println("Codigo desconocido , algo ha fallado.")
+    }
+
+    val tallaDeZapatos = 41
+    val mensaje = when(tallaDeZapatos){
+        41 , 43 -> "Tenemos Disponible"
+        42 , 44 -> "Casi no nos quedan!"
+        45 -> "Lo siento no tenemos disponible"
+        else -> "Estos Zapatos solo vienen en tallas 41 , 42 , 43 , 44 y 45"
     }
 
     println(mensaje)
