b464f8c1f5a15e05e908c2f2e7491a8a55413e85
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-06-01 19:31:26 -0500

    Clase 16

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,15 @@
 package org.example
 
 fun main(args: Array<String>){
-    val nombreColor = "Carmesi"
-
-    when (nombreColor) {
-        "Amarillo" -> println("EL amarillo es el color de la alegria")
-        "Rojo" , "Carmesi" -> println("Este color simboliza el calor")
-        else -> println("Error. No tengo informacion del color")
+    var contador = 10
+    while(contador > 0) {
+        println("El valor de contador es $contador")
+        contador--
     }
 
-    val code = 200
-    when(code){
-        in 200..299 -> println("Todo ha ido bien")
-        in 400..500 -> println("Algo ha fallado")
-        else -> println("Codigo desconocido , algo ha fallado.")
-    }
-
-    val tallaDeZapatos = 41
-    val mensaje = when(tallaDeZapatos){
-        41 , 43 -> "Tenemos Disponible"
-        42 , 44 -> "Casi no nos quedan!"
-        45 -> "Lo siento no tenemos disponible"
-        else -> "Estos Zapatos solo vienen en tallas 41 , 42 , 43 , 44 y 45"
-    }
-
-    println(mensaje)
+    do{
+        println("Generando numero aleatorio...")
+        val numeroAleatorio = (0..100).random()
+        println("El numero generado es $numeroAleatorio")
+    }while(numeroAleatorio > 50)
 }
