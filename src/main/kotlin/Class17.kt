a3575e53c331f907cc529c3cdebe7cc25da04e30
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-06-10 14:51:59 -0500

    Class 19

--- src/main/kotlin/Class17.kt
+++ src/main/kotlin/Class17.kt
@@ -1,2 +1,19 @@
 package org.example
 
+// Class 19
+
+fun main(args: Array<String>){
+    var nombre : String? = null
+    try{
+        throw NullPointerException("Referencia nula")
+    } catch(excepcion : NullPointerException){
+        println("HA ocurrido un error")
+    }finally {
+        println("Finalmente ha ocurrido un error... Cerrando aplicacion")
+    }
+
+    val primerValor = 10
+    val segundoValor = 0
+    val resultado : Int = try { primerValor / segundoValor } catch (exception : Exception) {0}
+    println(resultado)
+}
