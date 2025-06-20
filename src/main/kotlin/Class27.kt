7431879e37f7e1c9bb09a722ce9b0e7b27fb76ee
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-06-20 13:21:23 -0500

    Class27 Poner parametros y los demas archivos

---
+++ src/main/kotlin/Class27.kt
@@ -0,0 +1,9 @@
+package org.example
+
+fun main(args: Array<String>){
+    imprimirNombre(nombre = "Samuel" , apellido = "Pérez")
+}
+
+fun imprimirNombre(nombre: String, apellido: String){
+    println("Mi nombre completo es $nombre  $apellido")
+}
